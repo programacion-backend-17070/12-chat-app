f0c14ec61666046174aea9ca6a5d2086cac62947
lalomx <lalo0892@gmail.com>
2022-02-15 17:44:42 -0600

    template terminado

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,26 @@
-const socket = io()
+// const socket = io()
+const modalEl = document.getElementById("modal-full")
+const inputNameEl = document.getElementById("input-name")
+const chatContainerEl = document.getElementById("chat-container")
+
+UIkit.modal(modalEl).show();
+UIkit.util.on("#modal-full", "hidden", () => {
+  chatContainerEl.classList.toggle("hidden")
+  document.getElementById("username").innerText = user.name
+})
+
+inputNameEl.addEventListener("keyup", (e) => {
+
+  if (e.code === "Enter") {
+    e.preventDefault();
+    // Trigger the button element with a click
+    if (!e.target.value) {
+      return
+    }
+
+    user.name = e.target.value
+    UIkit.modal(modalEl).hide();
+  }
+})
+
+
