711f4a92f6f654f7812105801392e078a1756c28
lalomx <lalo0892@gmail.com>
2022-02-15 19:53:46 -0600

    listo para arrancar la clase

--- app.js
+++ app.js
@@ -1,51 +1,13 @@
 const express = require('express');
 const path = require('path')
 const http = require('http')
-const { Server } = require('socket.io');
 
 const app = express()
 const server = http.createServer(app)
-const io = new Server(server)
 
 app.use("/static", express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
 
-const users = {}
-
-io.on('connection', (socket) => {
-  // cuando un nueva connection llega al server
-  console.log(`an user connected: ${socket.id}`)
-  
-  // callback para saber cuando se ha desconectado
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-
-    // borramos el usuario de la lista
-    // emitir el evento de desconexion
-    delete users[socket.id]
-    socket.broadcast.emit("offline", socket.id)
-  });
-
-  // callback cuando el usuario se 'presenta'
-  // guardamos el nombre en el pool de usuarios
-  socket.on("iam", (name) => {
-    users[socket.id] = name
-
-    // una vez que sabemos el nombre del usuario se le transmite 
-    // el pool actual de usuarios conectados
-    for (const u of Object.entries(users)) {
-      socket.emit("users", { id: u[0], name: u[1] })
-    }
-
-    socket.broadcast.emit("users", { id: socket.id, name })
-  })
-
-  // recibimos un mensaje y lo redirigimos a los demas
-  socket.on("message", (message) => {
-    console.log(message)
-    socket.broadcast.emit("message", message)
-  })
-})
 
 server.listen(8080, () => console.log(`listening on http://localhost:8080`))
--- public/js/chat.js
+++ public/js/chat.js
@@ -6,7 +6,6 @@ const messageInput = document.getElementById("message-input")
 const sendBtn = document.getElementById("send-btn")
 const msgPool = document.getElementById("message-pool")
 const usrList = document.getElementById("user-list")
-const groupList = document.getElementById("group-list")
 
 const user = {}
 const users = []
@@ -22,46 +21,6 @@ UIkit.util.on("#modal-full", "hidden", () => {
   chatContainerEl.classList.toggle("hidden")
   document.getElementById("username").innerText = user.name
   msgPool.innerHTML = null
-
-  // conectar con socket io
-  user.socket = io()
-
-  // escuchar diferentes eventos
-
-  // enviamos nuestro nombre de usuario al server para que los demas nos conozcan
-  user.socket.emit("iam", user.name)
-
-  // escuchar la lista de usuarios conectados actualmente
-  user.socket.on("users", renderUser)
-
-  // escuchar cuando un usuario se ha desconectado
-  user.socket.on("offline", deleteUser)
-
-  // escuchar cuando un nuevo mensaje ha sido recibido
-  user.socket.on("message", render)
-
-  // resetar la lista de usuarios, poniendo siempre al inicio al usuario de la app
-  resetUserList()
-})
-
-// callback para enviar mensaje cuando damos click  en el boton
-sendBtn.addEventListener("click", (e) => {
-  e.preventDefault()
-  if (!messageInput.value) {
-    return
-  }
-
-  const message = {
-    message: messageInput.value,
-    date: Date.now(),
-    user: user.name,
-    destination: ""
-  }
-
-  // enviamos el mensaje y renderizamos el mismo en la lista de mensajes
-  user.socket.emit("message", message)
-  render(message)
-  messageInput.value = null
 })
 
 // callback para saber cuando el usuario ha ingresado su nombre y cerrar el modal
@@ -76,48 +35,3 @@ inputNameEl.addEventListener("keyup", (e) => {
     UIkit.modal(modalEl).hide();
   }
 })
-
-// rendizar un mensaje
-// distinguiendo entre mensaje enviado o mensaje recibido
-function render(data) {
-  const msgElement = document.createElement("div")
-  const userEl = `<span class="user">${data.user}</span>`
-  const timeEl = `<span class="date-time">${new Date(data.date).toLocaleString()}</span> &nbsp;`
-  const cssClass = data.user == user.name ? "local" : "remote"
-  msgElement.classList.add(cssClass)
-  msgElement.innerHTML = `
-    <div class="message-data uk-text-small ${cssClass === "local" ? "align-right" : ""}">
-      ${cssClass === "local" ? userEl + timeEl : timeEl + userEl}
-    </div>
-    <div class="message-body">${data.message}</div>
-  `
-  msgPool.appendChild(msgElement)
-}
-
-// renderizar un usuario en la lista
-function renderUser(u) {
-  if (u.name == user.name) {
-    return
-  }
-  users.push(u)
-  const liEl = document.createElement("li")
-  liEl.innerHTML = u.name
-  liEl.dataset.id = u.id
-  usrList.appendChild(liEl)
-}
-
-// borrar un usuario de la lista
-function deleteUser(id) {
-  resetUserList()
-  users.filter(u => u.id !== id).forEach(renderUser)
-}
-
-
-// inicializar la lista de usuarios
-function resetUserList() {
-  usrList.innerHTML = null
-
-  const meEl = document.createElement("li")
-  meEl.innerHTML = "🙋🏻‍♂️"
-  usrList.appendChild(meEl)
-}
