df11a2bd0e01738eb555fab57d14fbb1fe678edd
lalomx <lalo0892@gmail.com>
2022-02-16 08:05:57 -0600

    clase en vivo

--- app.js
+++ app.js
@@ -1,13 +1,56 @@
 const express = require('express');
 const path = require('path')
 const http = require('http')
+const { Server } = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
+const io = new Server(server)
 
 app.use("/static", express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
 
+const users = {}
+const msg = [] // base de datos de mensajes ( es un array :P)
+
+io.on("connection", (socket) => {
+  // cuando un nueva connection llega al server
+  console.log(`an user connected: ${socket.id}`)
+
+  socket.on("iam", (name) => {
+    users[socket.id] = name
+
+    console.log(users)
+    // for of
+    // for in
+    // Object.entries => [ [key, value] ]
+    for (const u of Object.entries(users)) {
+      // u => [key, value]
+
+      // enviar al usuario recien conectado
+      // los usuarios actualmente en linea
+      // {
+      //   id: socket.id,
+      //   name: nombre de usuario
+      // }
+      socket.emit("users", { id: u[0], name: u[1] })
+    }
+
+    // emitir el evento de que un nuevo usuario esta en linea
+    socket.broadcast.emit("users", { id: socket.id, name })
+
+    for (const m of msg) {
+      socket.emit("message", m)
+    }
+  })
+
+  socket.on("message", (data) => {
+    console.log(data)
+    msg.push(data)
+    // retransmitir mensaje
+    socket.broadcast.emit("message", data)
+  })
+})
 
 server.listen(8080, () => console.log(`listening on http://localhost:8080`))
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,11 @@
 // obtenemos todos los elementos de la UI que vamos a manipular
-const modalEl = document.getElementById("modal-full")
-const inputNameEl = document.getElementById("input-name")
+const modalEl = document.getElementById("modal-full") // modal
+const inputNameEl = document.getElementById("input-name") // input del modal
 const chatContainerEl = document.getElementById("chat-container")
-const messageInput = document.getElementById("message-input")
-const sendBtn = document.getElementById("send-btn")
-const msgPool = document.getElementById("message-pool")
-const usrList = document.getElementById("user-list")
+const messageInput = document.getElementById("message-input") // input del chat
+const sendBtn = document.getElementById("send-btn") // boton de enviar
+const msgPool = document.getElementById("message-pool") // lista de mensajes
+const usrList = document.getElementById("user-list") // lista de usuarios
 
 const user = {}
 const users = []
@@ -21,6 +21,18 @@ UIkit.util.on("#modal-full", "hidden", () => {
   chatContainerEl.classList.toggle("hidden")
   document.getElementById("username").innerText = user.name
   msgPool.innerHTML = null
+
+  // iniciar socket io
+  user.socket = io()
+
+  // se envia el nombre de usuario
+  user.socket.emit("iam", user.name)
+
+  user.socket.on("users", renderUser)
+
+  user.socket.on("message", render) // nuevo mensaje recibido
+
+  resetUserList()
 })
 
 // callback para saber cuando el usuario ha ingresado su nombre y cerrar el modal
@@ -31,7 +43,60 @@ inputNameEl.addEventListener("keyup", (e) => {
       return
     }
 
-    user.name = e.target.value
-    UIkit.modal(modalEl).hide();
+    user.name = e.target.value // guardar nombre de usuario en un objeto global
+    UIkit.modal(modalEl).hide(); // cerrar el modal
   }
 })
+
+sendBtn.addEventListener("click", (e) => {
+  e.preventDefault()
+  if (!messageInput.value) {
+    return
+  }
+
+  const message = {
+    message: messageInput.value,
+    date: Date.now(),
+    user: user.name
+  }
+
+  user.socket.emit("message", message) // ya envie el mensaje
+  render(message)
+  messageInput.value = null
+})
+
+function render(data) {
+  const msgElement = document.createElement("div")
+  const userEl = `<span class="user">${data.user}</span>`
+  const timeEl = `<span class="date-time">${new Date(data.date).toLocaleString()}</span> &nbsp;`
+  const cssClass = data.user == user.name ? "local" : "remote"
+  msgElement.innerHTML = `
+    <div class="message-data uk-text-small ${cssClass === "local" ? "align-right" : ""}">
+      ${cssClass === "local" ? userEl + timeEl : timeEl + userEl}
+    </div>
+    <div class="message-body">${data.message}</div>
+  `
+
+  msgPool.appendChild(msgElement)
+  msgPool.scrollTop = msgPool.scrollHeight // scroll hasta abajo
+}
+
+function renderUser(u) {
+  if (u.name == user.name) {
+    return
+  }
+
+  users.push(u)
+
+  const liEl = document.createElement("li")
+  liEl.innerHTML = u.name
+  usrList.appendChild(liEl)
+}
+
+function resetUserList() {
+  usrList.innerHTML = null
+
+  const meEl = document.createElement("li") // <li>🙋🏻‍♂️</li>
+  meEl.innerHTML = "🙋🏻‍♂️"
+  usrList.appendChild(meEl)
+}
