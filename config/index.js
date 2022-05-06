72d1add6b40dc59edaeeb9244a66b681ba98edb9
lalomx <lalo0892@gmail.com>
2022-05-06 08:29:41 -0500

    nuevo usuario

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   mongoConfig: {
     HOSTNAME: "cluster0.go6w7.mongodb.net",
     SCHEMA: "mongodb+srv",
-    USER: "lalomx",
+    USER: "chat",
     PASSWORD: process.env.MONGO_PASSWORD,
     DATABASE: "chat",
     OPTIONS: "retryWrites=true&w=majority"
